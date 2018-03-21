efb10ffb7b1ad688baf4b205841c6f0804d88650
Matt Miller and Nick Kardish <nick@>
2018-03-21 13:18:20 -0700

    clean up scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,10 @@ $(document).ready(function(){
   $("form#appointment").submit(function(event) {
     event.preventDefault();
     var name1 = $("#name1").val();
-    alert(name1);
-  });
-  $("form#appointment").submit(function(event) {
-    event.preventDefault();
     var description1 = $("#description1").val();
-    alert(description1);
-  });
-  $("form#appointment").submit(function(event) {
-    event.preventDefault();
     var date1 = $("#date1").val();
-    alert(date1);
-  });
-  $("form#appointment").submit(function(event) {
-    event.preventDefault();
     var startEndTime1 = $("#startEndTime1").val();
-    alert(startEndTime1);
+    alert(name1 + description1 + date1 + startEndTime1);
   });
+
 });
